9e57b468d91c1c21f3e0bf6deb72046b61fb5f1c
Wilder Pereira <wilder_roberto@hotmail.com>
2018-06-19 00:38:09 -0300

    Add routes to auth and institution services

--- src/main/kotlin/fiscaluno/gateway/GatewayApplication.kt
+++ src/main/kotlin/fiscaluno/gateway/GatewayApplication.kt
@@ -2,9 +2,31 @@ package fiscaluno.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+
 
 @SpringBootApplication
-class GatewayApplication
+class GatewayApplication {
+    @Bean
+    fun configureRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+                .route { it.path("/user/auth").uri("https://fiscaluno-mu.herokuapp.com/users") }
+
+                .route { it.path("/users").uri("https://crudgo.herokuapp.com/users") }
+
+                .route { it.path("/institutions").uri("https://fiscaluno-hyoga.herokuapp.com/") }
+
+                .route("intitution_path", {it.path("/institution/**")
+                        .filters { it.rewritePath("/institution/(?<ID>.*)","/\${ID}") }
+                        .uri("https://fiscaluno-hyoga.herokuapp.com")})
+
+
+                .build()
+
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
